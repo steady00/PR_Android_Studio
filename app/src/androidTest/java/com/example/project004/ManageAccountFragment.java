6753c54a0aeb0c085490bd95f47166737c75834b
steady00 <steady.tjiakra@gmail.com>
2022-02-03 21:22:20 +0700

    New Update

---
+++ app/src/androidTest/java/com/example/project004/ManageAccountFragment.java
@@ -0,0 +1,6 @@
+package com.example.project004;
+
+import androidx.fragment.app.Fragment;
+
+public class ManageAccountFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/project004/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.project004;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.project004.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initView();
+    }
+
+    //menampilkan titik tiga menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.custom_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    Fragment fragment = null;
+    // handler titik tiga menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                getSupportActionBar().setTitle("Home");
+                binding.drawer.closeDrawer(GravityCompat.START);
+                callFragment(fragment);
+                break;
+            case R.id.nav_contact_us:
+                fragment = new ContactUsFragment();
+                getSupportActionBar().setTitle("Contact Us");
+                binding.drawer.closeDrawer(GravityCompat.START);
+                callFragment(fragment);
+                break;
+            case R.id.nav_about_us:
+                fragment = new AboutUsFragment();
+                getSupportActionBar().setTitle("About Us");
+                binding.drawer.closeDrawer(GravityCompat.START);
+                callFragment(fragment);
+                break;
+            default:
+                Toast.makeText(this, "No Menu is selected", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void initView() {
+        // custom toolbar
+        setSupportActionBar(binding.toolbar);
+
+        //default fragment dibuka pertama kali
+        getSupportActionBar().setTitle("Home");
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit();
+        binding.navView.setCheckedItem(R.id.nav_home);
+
+        // membuka drawer
+        toggle = new ActionBarDrawerToggle(this, binding.drawer, binding.toolbar, R.string.open, R.string.close);
+
+        //drawer back button
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+
+        //sinkronisasi drawer
+        toggle.syncState();
+
+
+        // salah satu menu navigasi dipilih
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        getSupportActionBar().setTitle("Home");
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+                    case R.id.nav_contact_us:
+                        fragment = new ContactUsFragment();
+                        getSupportActionBar().setTitle("Contact Us");
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+                    case R.id.nav_about_us:
+                        fragment = new AboutUsFragment();
+                        getSupportActionBar().setTitle("About Us");
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+                    case R.id.btn_manage_account:
+                        fragment = new ManageAccountFragment();
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+                }
+                return true;
+            }
+
+        });
+
+    }
+
+    private void callFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.setCustomAnimations(
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right);
+        transaction.replace(R.id.frameLayout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
